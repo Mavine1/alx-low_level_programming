543fdb0c86b7b6725fa6c0bf3250f5b704c6ecc8
Mavine1 <mavinemalin@gmail.com>
2022-12-11 00:06:39 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,21 +3,16 @@
 /**
  * main-print all the data types
  * code by mavine
- * return: Always 0 (success
+ * Return: Always 0 (success
 */
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
 
-	printf("size of a char :%lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("size of a int  :%lu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("size of a long int  :%lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("size of a long long int  :%lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float :%lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("size of a char :%lu bytes(s)\n", sizeof(char));
+	printf("size of a int  :%lu bytes(s)\n", sizeof(int));
+	printf("size of a long int  :%lu bytes(s)\n", sizeof(long int));
+	printf("size of a long long int  :%lu bytes(s)\n", sizeof(long long int));
+	printf("size of a float :%lu bytes(s)\n", sizeof(float));
 	return (0);
 }
