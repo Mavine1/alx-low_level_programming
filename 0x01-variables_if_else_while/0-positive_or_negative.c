897baea0a24c9fbb51883bdd4e499e540578f976
Mavine1 <mavinemalin@gmail.com>
2022-12-12 10:09:30 -0800

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int main(void)
 
 	if (n < 0)
 	{
-		printf(" %d is postive\n", n);
+		printf(" %d is negative\n", n);
 	}
 	else if (n == 0)
 	{
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is postive\n", n);
 
 	}
 	return (0);
