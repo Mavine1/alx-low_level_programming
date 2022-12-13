1cb59c47e8208f6efa06c59e468e18f0d629c5c7
Mavine1 <mavinemalin@gmail.com>
2022-12-13 02:14:18 -0800

    fix

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints all the possible values 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int n;
+
+	for (i = 48; i < 58; n++)
+	{
+		for (n = 48
