11166ddf41a93c17b81c4833b8205ae8d21c14ca
Mavine1 <mavinemalin@gmail.com>
2022-12-14 05:47:19 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print - all aiphabet
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}
