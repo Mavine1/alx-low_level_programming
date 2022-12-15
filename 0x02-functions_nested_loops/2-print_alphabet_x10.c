ed7d4a9dbe4ee2530c608228915e8ee1628f84b4
Mavine1 <mavinemalin@gmail.com>
2022-12-15 05:43:00 -0800

    task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,16 @@
 #include "main.h"
 /**
+ * print- print alph 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int m;
+	char k;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+			_putchar(k);
+		-putchar('\n');
+	}
+}
