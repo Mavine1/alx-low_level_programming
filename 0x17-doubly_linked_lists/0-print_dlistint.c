962b3d07fb04bbf3e145e7f90ef5e0732525bcef
Mavine1 <mavinemalin@gmail.com>
2023-05-29 01:33:02 -0700

    new

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+/*
+ * Author: Mavine Muganda
+ */
+
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
